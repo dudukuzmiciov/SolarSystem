8453fea0491d226187ad4791e9f939803f4be36a
DANA CUZMICIOV <dudukuzmiciov@gmail.com>
2022-05-18 20:16:56 -0400

    Planet 2 features have been created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,5 +20,11 @@ public class Main {
         FacX.radius="7893645793";
         FacX.size="999999999";
 
+        //adding features for planet 2
+        Planet2 Olso = new Planet2();
+        Olso.name="Ozhan";
+        Olso.size="694254km";
+        Olso.color="pink";
+
     }
 }
